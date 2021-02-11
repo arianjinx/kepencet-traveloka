ec2e3ca38ebe7b01ae1a7b0c112272e9b653f873
Arian Pradana <arian.raptor@gmail.com>
2021-02-12 00:15:50 +0700

    new api endpoint and basic auth

--- utils/getRecentTweets.js
+++ utils/getRecentTweets.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 async function getRecentTweetsWithQuery() {
-  const res = await axios.get(`${process.env.TWITTER_API_URL.trim()}`);
+  const res = await axios.get(`${process.env.N8N_API_URL_KEPENCET_TRAVELOKA.trim()}`, {
+    auth: {
+      username: `${process.env.N8N_BASIC_AUTH_USER.trim()}`,
+      password: `${process.env.N8N_BASIC_AUTH_PASSWORD.trim()}`,
+    },
+  });
 
   if (res.data.errors) {
     console.error("errors", res.data.errors);
