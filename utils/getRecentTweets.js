b5bcd2e058d7224ddf93403b2aa880d0e2179fb5
Arian Pradana <arian.raptor@gmail.com>
2021-02-12 11:27:12 +0700

    reformat and retrigger

--- utils/getRecentTweets.js
+++ utils/getRecentTweets.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 async function getRecentTweetsWithQuery() {
-  const res = await axios.get(`${process.env.N8N_API_URL_KEPENCET_TRAVELOKA.trim()}`, {
-    auth: {
-      username: `${process.env.N8N_BASIC_AUTH_USER.trim()}`,
-      password: `${process.env.N8N_BASIC_AUTH_PASSWORD.trim()}`,
-    },
-  });
+  const res = await axios.get(
+    `${process.env.N8N_API_URL_KEPENCET_TRAVELOKA.trim()}`,
+    {
+      auth: {
+        username: `${process.env.N8N_BASIC_AUTH_USER.trim()}`,
+        password: `${process.env.N8N_BASIC_AUTH_PASSWORD.trim()}`,
+      },
+    }
+  );
 
   if (res.data.errors) {
     console.error("errors", res.data.errors);
